a9b2b50319988ff46f5929a9294d841101b098ad
Vamshi Krishna S <107762231+VamshiKrsna@users.noreply.github.com>
2024-10-02 22:59:51 +0530

    defined project template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medico.ai-project',
+    version='0.1.0',
+    description='This is a medical chatbot powered by RAG.',
+    author='Vamshi Krishna S',
+    packages= find_packages(),
+    install_requires=[]
+)
